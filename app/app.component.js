a56132d55177ee6fb212a1315993930973aafbb4
Yuxiang Zhang <Yuxiang Zhang>
2016-11-28 16:59:23 +1300

    Added 1.Two-way data binding for text box.<br/> 2.List of heros

--- app/app.component.js
+++ app/app.component.js
@@ -22,15 +22,28 @@ var AppComponent = (function () {
             id: 1,
             name: 'Windstorm'
         };
+        this.heros = HEROS;
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>{{title}}</h1> <h2>{{hero.name}} details</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label> {{hero.name}}</div>",
+            template: "\n    <h1>{{title}}</h1>\n    <h2>{{hero.name}} details</h2>\n    <div><label>id: </label>{{hero.id}}</div>\n    <div>\n      <label>name: </label>\n      <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n    </div>\n\n    <h2>My Heros</h2>\n    <ul class=\"heros\">\n      <li *ngFor=\"let hero of heros\">\n      <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var HEROS = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 //# sourceMappingURL=app.component.js.map
