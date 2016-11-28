d18781fe0b8d8cd8d4c526d7d5299e3083867e65
Yuxiang Zhang <Yuxiang Zhang>
2016-11-28 15:34:23 +1300

    Tutorial started

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+export class Hero {
+  id: number;
+  name: string;
+}
+@Component({
+  selector: 'my-app',
+  template: `<h1>{{title}}</h1> <h2>{{hero.name}} details</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label> {{hero.name}}</div>`,
+})
+export class AppComponent  {
+  title = 'Tour of Heros';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
