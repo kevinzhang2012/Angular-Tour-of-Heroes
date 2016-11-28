625727e53382383b42a888d884a79bfda448ca07
Yuxiang Zhang <kevinzhang2012@live.cn>
2016-11-28 21:15:13 +1300

    Added feature - select hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,24 +5,39 @@ export class Hero {
   name: string;
 }
 
-@Component({
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice'},
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
+@Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+      <li *ngFor="let hero of heroes"
+      [class.selected]="hero === selectedHero"
+      (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="selectedHero.name" placeholder="name">
+    </div>
+  </div>
     `,
     styles: [`
   .selected {
@@ -76,24 +91,10 @@ export class Hero {
 })
 export class AppComponent  {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'XXX'
-  };
   selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero) : void {
+    this.selectedHero = hero;
+  }
 }
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice'},
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
